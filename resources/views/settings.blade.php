63d79549454f9a563b1af71e17786de981822597
Your Name <you@example.com>
2021-10-02 18:25:17 +0200

    fuck it

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -4,41 +4,41 @@
 <div class="container">
     <div class=" justify-content-center">
 
-            <div class="card">
-                <div class="card-header">Settings list</div>
+        <div class="card">
+            <div class="card-header">Settings list</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-               
-<table class="table">
-    <thead>
-        <tr>
-            <th>Navn</th>
-            <th>Epost</th>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($settings as $setting)
-        <tr>
-            <td>
-            {{$setting->key}}
-            </td>
-            <td>
-            {{$setting->value}}
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
-                  
+            <div class="card-body">
+                @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+                @endif
+
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Setting</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($settings as $setting)
+                        <tr>
+                            <td>
+                                {{$setting->key}}
+                            </td>
+                            <td>
+                                {{$setting->value}}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
             </div>
-      
+        </div>
+
     </div>
 </div>
 @endsection
